7e741f6684cb7469501229b46ad9449702bd7cd1
saie04 <gholapsaie09a13@gmail.com>
2026-02-16 12:34:28 +0530

    modified code

--- ultrasonic_sensor.c
+++ ultrasonic_sensor.c
@@ -7,3 +7,18 @@ void setup(){
  pinMode(TRIG_PIN , OUTPUT);
  pinMode(ECHO_PIN , INPUT);
 }
+
+void loop() {
+  Serial.println("Ultrasonic Sensor HC-SRC04 Test");
+  digitalWrite(TRIG_PIN, LOW);
+  delayMicroseconds(20);
+  digitalWrite(TRIG_PIN, HIGH);
+  delayMicroseconds(100);
+  digitalWrite(TRIG_PIN, LOW);
+  duration = pulseIn(ECHO_PIN,HIGH);
+  distance = duration * 0.034 / 2;
+  Serial.print("Distance: ");
+  Serial.print(distance);
+  Serial.println(" cm");
+
+}
